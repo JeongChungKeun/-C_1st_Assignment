7ca9e54ba0a8a725f35e4cc8f33f93c430037af6
JeongChungKeun <31757999+JeongChungKeun@users.noreply.github.com>
2017-09-08 16:03:55 +0900

    C_1st_assignment.c
    C_1st_assignment

---
+++ C_1st_assignment.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("1. My student id number is 2017301070\n");
+	printf("2. My name is Jeong Chung Keun\n");
+	printf("3. My mobile phone number is 010 6472 0795\n");
+	printf("4. My GitHub address is https://github.com/JeongChungKeun\n");
+
+	return 0;
+}
